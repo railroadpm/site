ac183aa6c02706a6ad52df3767a1213c85a16265
SLathrop <steve@stela.io>
2018-05-23 09:32:40 -0400

    Image assets

--- app/pages/reports.vue
+++ app/pages/reports.vue
@@ -1,6 +1,8 @@
 <template>
   <v-layout>
     <v-flex>
+      <img src="https://app.rrpm.run/asset/bnsf_railway.jpg" alt="">
+      <br>
       <tabular-report entity-base-url="/reports/bnsf" entity-result-cols-key="columns" entity-result-rows-key="rows" />
     </v-flex>
   </v-layout>

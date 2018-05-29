32a0000ceecc376151d0873226e1cb2949d72007
SLathrop <steve@stela.io>
2018-05-29 02:43:10 -0400

    WIP Dataprise/ws#57 historical rpts

--- app/components/TabularReport.vue
+++ app/components/TabularReport.vue
@@ -1,16 +1,21 @@
 <template>
-  <v-data-table :must-sort="true" :headers="headers" :items="rows" hide-actions>
-    <template slot="items" slot-scope="row">
-      <!-- First render the row label cell -->
-      <td class="rpt-data-label">
-        <span :class="{ 'rpt-data-heading-row': row.item.isHeadingRow }">
-          <vue-markdown>{{ row.item["RowLabel"] }}</vue-markdown>
-        </span>
-      </td>
-      <!-- Then render a cell for each week + measure (week is all numeric: YYYYMMDD) in ascending order by week -->
-      <td v-for="(col, i) in Object.keys(row.item).sort().filter(k => !isNaN(k))" :key="i">{{ row.item[col] | formatNumber }}</td>
-    </template>
-  </v-data-table>
+  <div>
+    <div v-show="reportType === 'Historical'" class="text-xs-center rpt-pagination-ctl">
+      <v-pagination :circle="true" :length="numPages" :total-visible="numPages" v-model="page" color="blue darken-4"></v-pagination>
+    </div>
+    <v-data-table :headers="headers" :items="rows" hide-actions>
+      <template slot="items" slot-scope="row">
+        <!-- First render the row label cell -->
+        <td class="rpt-data-label">
+          <span :class="{ 'rpt-data-heading-row': row.item.isHeadingRow }">
+            <vue-markdown>{{ row.item["RowLabel"] }}</vue-markdown>
+          </span>
+        </td>
+        <!-- Then render a cell for each week + measure (week is all numeric: YYYYMMDD) in ascending order by week -->
+        <td v-for="(col, i) in measureKeys" :key="i">{{ row.item[col] | formatNumber }}</td>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -19,25 +24,39 @@ import numeral from 'numeral';
 
 export default {
   props: {
-    entityBaseUrl: {
+    railroad: {
+      type: String,
+      required: true
+    },
+    reportType: {
       type: String,
       required: true
     }
   },
 
   data: () => ({
+    page: 1, // These three page-related items are for "Historical" reports only
+    pageSize: 6,
+    numHistoricalPages: 9, // We'll group the 53 weeks into 8 "pages" of 6, with a 9th page having the 5 left over
+
     loading: true,
-    pagination: {
-      sortBy: 'RowLabelOrd'
-    },
-    mustSort: true,
+    rawColumns: [],
     headers: [],
-    rows: []
+    rows: [],
+    measureKeys: [] // Array of keys for lookup of measure data in rows, by week
   }),
+
+  watch: {
+    page: function(newPage, oldPage) {
+      if (newPage != oldPage) {
+        this.getHeadersAndMeasureKeysFromRawData(newPage);
+      }
+    }
+  },
 
   created() {
     console.log('Created <TabularReport> component');
-    this.getRows();
+    this.getTabularData();
   },
 
   mounted() {
@@ -46,33 +65,56 @@ export default {
 
   computed: {
     dataUrl() {
-      return `${app.API_HOST}${this.entityBaseUrl}/all${app.API_GET_SUFFIX}`;
+      return `${app.API_HOST}/reports/${this.railroad.toLowerCase()}/${this.reportType === 'Current' ? 'current' : 'all'}/${app.API_GET_SUFFIX}`;
+    },
+    numPages() {
+      if (this.reportType === 'Current') return 1;
+      return this.numHistoricalPages;
     }
   },
 
   methods: {
-    async getRows() {
+    async getTabularData() {
       this.loading = true;
 
       try {
-        console.log('Getting rows...');
+        console.log('Getting tabular data...');
         const response = await this.$axios.$get(this.dataUrl);
-        console.log(`Got rows from ${this.dataUrl}`);
+        console.log(`Got rows and columns from ${this.dataUrl}`);
 
-        let responseCols = response.data.columns;
+        this.rawColumns = response.data.columns;
         this.headers = [];
-        responseCols.forEach(elt => {
-          this.headers.push({ text: elt.text, value: elt.key, align: 'left' });
-        });
-
+        this.rows = [];
+        this.measureKeys = [];
+        this.getHeadersAndMeasureKeysFromRawData(this.page);
         this.rows = response.data.rows;
       } catch (e) {
         this.headers = [];
         this.rows = [];
-        console.log('Error getting rows:', e);
+        console.log('Error getting rows and columns:', e);
       }
 
       this.loading = false;
+    },
+    getHeadersAndMeasureKeysFromRawData(pageNum) {
+      let renderedCols = [];
+
+      if (this.reportType === 'Current') {
+        renderedCols = this.rawColumns;
+      } else {
+        // Note in this case we need to manually add the "RowLabel" col first...
+        renderedCols = this.rawColumns.slice((pageNum - 1) * this.pageSize + 1, (pageNum - 1) * this.pageSize + (this.pageSize + 1));
+        this.headers = [];
+        this.headers.push({ text: '', value: 'RowLabel', align: 'left', sortable: false });
+        this.measureKeys = [];
+      }
+
+      if (this.reportType === 'Historical' || this.headers.length === 0) {
+        renderedCols.forEach(elt => {
+          this.headers.push({ text: elt.text, value: elt.key, align: 'left', sortable: false });
+          if (!isNaN(elt.key)) this.measureKeys.push(elt.key);
+        });
+      }
     }
   },
 
@@ -97,7 +139,7 @@ export default {
 
 /* Table header */
 table.datatable.table thead tr {
-  height: 45px;
+  height: 38px;
 }
 
 /* Table header column */
@@ -109,6 +151,12 @@ table.datatable.table thead tr th {
 /* Table rows */
 table.datatable.table tbody td,
 table.datatable.table tbody th {
-  height: 35px;
+  height: 23px;
+}
+
+.rpt-pagination-ctl {
+  padding: 15px 0 10px 0;
+  width: 100%;
+  background-color: white;
 }
 </style>

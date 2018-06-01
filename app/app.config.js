dc67f461790b639c6d18d649cc90caa245def414
SLathrop <steve@stela.io>
2018-06-01 11:46:30 -0400

    Prod API host

--- app/app.config.js
+++ app/app.config.js
@@ -1,7 +1,7 @@
 const pkg = require('./package')
 
 module.exports = {
-  // API_HOST: 'https://api.rrpm.run',
-  API_HOST: 'http://localhost:1313',
+  API_HOST: 'https://api.rrpm.run',
+  // API_HOST: 'http://localhost:1313',
   API_GET_SUFFIX: '/get.json'
 }

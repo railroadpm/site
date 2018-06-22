17a6f78637177734b7a89ff1e827f48a7924dd85
SLathrop <steve@stela.io>
2018-06-22 15:23:23 -0400

    WIP Dataprise/ws#44 build logic, defensive Hugo build

--- app/app.config.js
+++ app/app.config.js
@@ -1,8 +1,8 @@
 const pkg = require('./package');
 
 module.exports = {
-  API_HOST: 'https://api.rrpm.run',
-  // API_HOST: 'http://localhost:1313',
+  // API_HOST: 'https://api.rrpm.run',
+  API_HOST: 'http://localhost:1313',
   API_GET_SUFFIX: '/get.json',
   CSV_GET_SUFFIX: '/get.csv'
 };
